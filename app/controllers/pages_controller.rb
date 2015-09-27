79c70f26812dcbf7b1fd9c9af8945b7db930bd5f
Patrick Maciel <patrickmaciel.info@gmail.com>
2015-09-27 16:25:59 -0300

    Added: Configured the templates for each page type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,18 @@ class PagesController < ContentController
   def show
     @page = Page.find params[:id]
 
-    render template: "page"
+    render template: template_to_render
+  end
+
+  protected
+
+  def template_to_render
+    if template_exists?("page-#{@page.id}")
+      "page-#{@page.id}"
+    elsif template_exists?("page-#{@page.type.name}")
+      "page-#{@page.type.name}"
+    else
+      "page"
+    end
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -5,4 +5,13 @@ class Page < ActiveRecord::Base
   has_many :fields
 
   accepts_nested_attributes_for :fields, reject_if: :all_blank
+
+  def method_missing name, *args, &block
+    field = self.fields.find { |field| field.field_definition.key == name.to_s }
+    if field
+      field.value
+    else
+      super
+    end
+  end
 end
